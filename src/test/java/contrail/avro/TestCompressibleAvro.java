b1e926088d3465aadf48e1a9b90ca0801477485f
Jeremy Lewi <jeremy@lewi.us>
2012-04-25 08:53:09 -0700

    Merge compressible_cl Conflicts: 	src/main/java/contrail/avro/CompressibleAvro.java

--- src/test/java/contrail/avro/TestCompressibleAvro.java
+++ src/test/java/contrail/avro/TestCompressibleAvro.java
@@ -2,6 +2,7 @@ package contrail.avro;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,8 +10,12 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
 import org.apache.avro.mapred.AvroCollector;
 import org.apache.avro.mapred.Pair;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.Reporter;
 import org.junit.Test;
@@ -292,5 +297,63 @@ public class TestCompressibleAvro {
       
       assertReduceOutput(case_data, collector_mock);
     }    
+  }
+  
+  @Test
+  public void testRun() {
+    SimpleGraphBuilder builder = new SimpleGraphBuilder();
+    builder.addKMersForString("ACTGG", 3);
+    
+    File temp = null;
+    
+    try {
+      temp = File.createTempFile("temp", Long.toString(System.nanoTime()));
+    } catch (IOException exception) {
+      fail("Could not create temporary file. Exception:" + 
+          exception.getMessage());
+    }
+    if(!(temp.delete())){
+        throw new RuntimeException(
+            "Could not delete temp file: " + temp.getAbsolutePath());
+    }
+
+    if(!(temp.mkdir())) {
+        throw new RuntimeException(
+            "Could not create temp directory: " + temp.getAbsolutePath());
+    }
+
+    File avro_file = new File(temp, "graph.avro");
+    
+    // Write the data to the file.
+    Schema schema = (new GraphNodeData()).getSchema();
+    DatumWriter<GraphNodeData> datum_writer = 
+        new SpecificDatumWriter<GraphNodeData>(schema);
+    DataFileWriter<GraphNodeData> writer = 
+        new DataFileWriter<GraphNodeData>(datum_writer); 
+    
+    try {
+      writer.create(schema, avro_file);
+      for (GraphNode node: builder.getAllNodes().values()) {
+        writer.append(node.getData());
+      }
+      writer.close();
+    } catch (IOException exception) {
+      fail("There was a problem writing the graph to an avro file. Exception:" + 
+          exception.getMessage());
+    }
+     
+    // Run it.
+    CompressibleAvro compressible = new CompressibleAvro();
+    File output_path = new File(temp, "output");
+    
+    String[] args = 
+      {"--inputpath=" + temp.toURI().toString(), 
+       "--outputpath=" + output_path.toURI().toString()};
+    
+    try {
+      compressible.run(args);
+    } catch (Exception exception) {
+      fail("Exception occured:" + exception.getMessage());
+    }
   }
 }
