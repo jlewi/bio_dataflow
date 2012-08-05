7d0f25503806a8eb05196e7c7fb8ca95485fd9f0
Jeremy Lewi <jeremy@lewi.us>
2012-08-05 15:45:44 -0700

    Test indicates a bug in Avro Pair.equals.

--- src/test/java/contrail/avro/TestAssertEquals.java
+++ src/test/java/contrail/avro/TestAssertEquals.java
@@ -2,31 +2,15 @@ package contrail.avro;
 
 import static org.junit.Assert.*;
 
+import org.apache.avro.mapred.Pair;
 import org.junit.Test;
-
-import contrail.RemoveTipMessage;
-import contrail.graph.GraphNode;
-import contrail.sequences.DNAAlphabetFactory;
-import contrail.sequences.Sequence;
-import contrail.sequences.StrandsForEdge;
 
 public class TestAssertEquals {
 
   @Test
   public void test() {
-    // Don't commit this code to the main repo. It was for a oneoff test.
-    GraphNode node = new GraphNode();
-    node.setNodeId("node");
-    node.setSequence(new Sequence("ACT", DNAAlphabetFactory.create()));
-    RemoveTipMessage msg1 = new RemoveTipMessage();
-    msg1.setEdgeStrands(StrandsForEdge.FF);
-    msg1.setNode(node.clone().getData());
-
-    RemoveTipMessage msg2 = new RemoveTipMessage();
-    msg2.setEdgeStrands(StrandsForEdge.FR);
-    msg2.setNode(node.clone().getData());
-
-    assertEquals(msg1, msg2);
-
+    Pair<Integer, Integer> pair1 = new Pair<Integer, Integer>(1, 2);
+    Pair<Integer, Integer> pair2 = new Pair<Integer, Integer>(1, 1);
+    assertFalse(pair1.equals(pair2));
   }
 }
