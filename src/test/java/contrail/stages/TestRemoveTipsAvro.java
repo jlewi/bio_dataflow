2b46e29e40ba9f3bfe873eccf723af800c64becf
Jeremy Lewi <jeremy@lewi.us>
2012-10-05 11:47:28 -0700

    1. Avoid filtering out islands that are long. 2. Cleanup TestRemoveTipsAvro style a bit.

--- src/test/java/contrail/stages/TestRemoveTipsAvro.java
+++ src/test/java/contrail/stages/TestRemoveTipsAvro.java
@@ -29,6 +29,8 @@ import contrail.ReporterMock;
 import contrail.graph.GraphNode;
 import contrail.graph.GraphNodeData;
 import contrail.graph.SimpleGraphBuilder;
+import contrail.sequences.DNAAlphabetFactory;
+import contrail.sequences.Sequence;
 import contrail.sequences.StrandsForEdge;
 
 public class TestRemoveTipsAvro extends RemoveTipsAvro{
@@ -37,11 +39,13 @@ public class TestRemoveTipsAvro extends RemoveTipsAvro{
    * Check the output of the map is correct.
    */
   private void assertMapperOutput(
-      GraphNodeData expected_node, Pair<String, RemoveTipMessage> expected_message,
+      GraphNodeData expected_node,
+      Pair<CharSequence, RemoveTipMessage> expected_message,
       AvroCollectorMock<Pair<CharSequence, RemoveTipMessage>> collector_mock) {
 
     // Check the output.
-    Iterator<Pair<CharSequence, RemoveTipMessage>> it = collector_mock.data.iterator();
+    Iterator<Pair<CharSequence, RemoveTipMessage>> it =
+        collector_mock.data.iterator();
 
     assertTrue(it.hasNext());
     Pair<CharSequence, RemoveTipMessage> actual_message = it.next();
@@ -52,11 +56,11 @@ public class TestRemoveTipsAvro extends RemoveTipsAvro{
   // Store the data for a particular test case for the map phase.
   private static class MapTestCaseData {
     public GraphNodeData node;
-    public Pair<String, RemoveTipMessage> expected_message;
+    public Pair<CharSequence, RemoveTipMessage> expected_message;
+    public int tiplength = 4;
   }
 
   private List<MapTestCaseData> constructMapCases() {
-
     SimpleGraphBuilder graph = new SimpleGraphBuilder();
     graph.addEdge("AAATC", "TCA", 2);
     graph.addEdge("ATC", "TCA", 2);
@@ -67,7 +71,8 @@ public class TestRemoveTipsAvro extends RemoveTipsAvro{
     // ADDING Non-Tip to casedata
     {
       MapTestCaseData non_tip= new MapTestCaseData();
-      Pair<String, RemoveTipMessage> expected_non_tip= new Pair<String, RemoveTipMessage>(MAP_OUT_SCHEMA);
+      Pair<CharSequence, RemoveTipMessage> expected_non_tip =
+          new Pair<CharSequence, RemoveTipMessage>(MAP_OUT_SCHEMA);
       GraphNode non_tip_node = graph.getNode(graph.findNodeIdForSequence("TCA"));
 
       RemoveTipMessage non_tip_msg = new RemoveTipMessage();
@@ -77,14 +82,17 @@ public class TestRemoveTipsAvro extends RemoveTipsAvro{
 
       non_tip.node= non_tip_node.getData();
       non_tip.expected_message= expected_non_tip;
+      non_tip.tiplength = 4;
       cases.add(non_tip);
     }
     // tiplength is 4 for this case
     // ADDING Non-Tip to casedata; AAATC gets identified as NON tip as its len is > 4
     {
       MapTestCaseData non_tip= new MapTestCaseData();
-      Pair<String, RemoveTipMessage> expected_non_tip= new Pair<String, RemoveTipMessage>(MAP_OUT_SCHEMA);
-      GraphNode non_tip_node = graph.getNode(graph.findNodeIdForSequence("AAATC"));
+      Pair<CharSequence, RemoveTipMessage> expected_non_tip =
+          new Pair<CharSequence, RemoveTipMessage>(MAP_OUT_SCHEMA);
+      GraphNode non_tip_node = graph.getNode(
+          graph.findNodeIdForSequence("AAATC"));
 
       RemoveTipMessage non_tip_msg = new RemoveTipMessage();
       non_tip_msg.setNode(non_tip_node.getData());
@@ -93,17 +101,19 @@ public class TestRemoveTipsAvro extends RemoveTipsAvro{
 
       non_tip.node= non_tip_node.getData();
       non_tip.expected_message= expected_non_tip;
-
+      non_tip.tiplength = 4;
       cases.add(non_tip);
     }
     // ADDING Tip to casedata
     // ATC is a tip node
     {
       MapTestCaseData tip= new MapTestCaseData();
-      Pair<String, RemoveTipMessage> expected_tip= new Pair<String, RemoveTipMessage>(MAP_OUT_SCHEMA);
+      Pair<CharSequence, RemoveTipMessage> expected_tip =
+          new Pair<CharSequence, RemoveTipMessage>(MAP_OUT_SCHEMA);
       GraphNode tip_node = graph.getNode(graph.findNodeIdForSequence("ATC"));
 
-      String terminal_nodeId = graph.getNode(graph.findNodeIdForSequence("TCA")).getNodeId();
+      String terminal_nodeId =
+          graph.getNode(graph.findNodeIdForSequence("TCA")).getNodeId();
       RemoveTipMessage tip_msg = new RemoveTipMessage();
       tip_msg.setNode(tip_node.getData());
       tip_msg.setEdgeStrands(StrandsForEdge.FF);
@@ -112,14 +122,37 @@ public class TestRemoveTipsAvro extends RemoveTipsAvro{
       tip.node= tip_node.getData();
       tip.expected_message= expected_tip;
 
+      tip.tiplength = 4;
       cases.add(tip);
     }
     return cases;
   }
 
+  private MapTestCaseData constructMapKeepIsland() {
+    // Construct a test case consisting of a GraphNode which is an island.
+    // In this case the island has a length longer than the tiplength so
+    // the island should not be removed.
+    MapTestCaseData testCase = new MapTestCaseData();
+
+    GraphNode node = new GraphNode();
+    node.setNodeId("island");
+    node.setSequence(new Sequence("GCTGACCCTA", DNAAlphabetFactory.create()));
+
+    testCase.node = node.getData();
+    testCase.tiplength = 4;
+
+    RemoveTipMessage message= new RemoveTipMessage();
+    message.setNode(node.clone().getData());
+    message.setEdgeStrands(null);
+
+    testCase.expected_message =
+        new Pair<CharSequence, RemoveTipMessage>(node.getNodeId(), message);
+
+    return testCase;
+  }
+
   @Test
   public void testMap() {
-
     ReporterMock reporter_mock = new ReporterMock();
     Reporter reporter = reporter_mock;
 
@@ -127,15 +160,15 @@ public class TestRemoveTipsAvro extends RemoveTipsAvro{
 
     RemoveTipsAvro stage= new RemoveTipsAvro();
     Map<String, ParameterDefinition> definitions = stage.getParameterDefinitions();
-    int tiplength= 4;
     JobConf job = new JobConf(RemoveTipsAvro.RemoveTipsAvroMapper.class);
-    definitions.get("tiplength").addToJobConf(job, new Integer(tiplength));
-    mapper.configure(job);
 
     // Construct the different test cases.
     List <MapTestCaseData> test_cases = constructMapCases();
-
+    test_cases.add(constructMapKeepIsland());
     for (MapTestCaseData case_data : test_cases) {
+      definitions.get("tiplength").addToJobConf(
+          job, new Integer(case_data.tiplength));
+      mapper.configure(job);
 
       AvroCollectorMock<Pair<CharSequence, RemoveTipMessage>>
       collector_mock =  new AvroCollectorMock<Pair<CharSequence, RemoveTipMessage>>();
