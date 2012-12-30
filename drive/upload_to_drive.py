77f4ec6128d66b35254b1065d448a5c059f2c49d
Jeremy Lewi <jeremy@lewi.us>
2012-12-29 17:16:04 -0800

    Fetch token from a file.

--- drive/upload_to_drive.py
+++ drive/upload_to_drive.py
@@ -15,6 +15,7 @@ import pprint
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
 from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.file import Storage
 
 import gflags
 import os
@@ -39,6 +40,17 @@ OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
 
+def NewCredentials():
+  """Get a new set of credentials."""
+  # Run through the OAuth flow and retrieve credentials
+  flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+  authorize_url = flow.step1_get_authorize_url()
+  print 'Go to the following link in your browser: ' + authorize_url
+  code = raw_input('Enter verification code: ').strip()
+  credentials = flow.step2_exchange(code)
+  return credentials
+
+
 def main(argv):
   try:
     unparsed = FLAGS(argv)  # parse flags
@@ -49,50 +61,59 @@ def main(argv):
     print "%s" % e
     print usage.format(name=argv[0], flags=FLAGS)
     sys.exit(1)
-  
+
   if not FLAGS.path:
     raise Exception("You must specify a file to upload using the --path argument.")
-        
-  # Run through the OAuth flow and retrieve credentials
-  flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
-  authorize_url = flow.step1_get_authorize_url()
-  print 'Go to the following link in your browser: ' + authorize_url
-  code = raw_input('Enter verification code: ').strip()
-  credentials = flow.step2_exchange(code)
-  
+
+  # Credential store.
+  # TODO(jeremy@lewi.us): We should make sure the credentials file is only readable by
+  # the user.
+  credentials_file = os.path.join(os.path.expanduser("~"), ".upload_to_drive")
+  storage = Storage(credentials_file)
+
+  # TODO(jeremy@lewi.us): How can we detect when the credentials have expired and
+  # refresh.
+  if os.path.exists(credentials_file):
+    # Load the credentials from the file
+    credentials = storage.get()
+  else:
+    # get new credentials
+    credentials = NewCredentials()
+    storage.put(credentials)
+
   # Create an httplib2.Http object and authorize it with our credentials
   http = httplib2.Http()
   http = credentials.authorize(http)
-  
+
   drive_service = build('drive', 'v2', http=http)
-    
+
   files = []
-  
+
   if os.path.isdir(FLAGS.path):
     for f in os.listdir(FLAGS.path):
       files.append(os.path.join(FLAGS.path, f))
   else:
     files.append(FLAGS.path)
-    
+
   for file_path in files:
     # Insert a file
     media_body = MediaFileUpload(file_path, mimetype='text/plain', resumable=True)
     mime_type = "text/plain"
-    
+
     file_ext = os.path.splitext(file_path)[1].lower()
     if file_ext == ".html":
       mime_type = "text/html"
-      
+
     name = os.path.basename(file_path)
-    
+
     body = {
       'title': name,
       'description': FLAGS.description,
       'mimeType': mime_type
     }
-  
+
     file = drive_service.files().insert(body=body, media_body=media_body, convert=True).execute()
     pprint.pprint(file)
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
   main(sys.argv)
