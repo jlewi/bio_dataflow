cab29796b812f5d1ecbe9f0be258a993be25faf1
Jeremy Lewi <jeremy@lewi.us>
2013-01-06 10:11:19 -0800

    Minor cleanup.

--- src/main/java/contrail/correct/FastQtoAvro.java
+++ src/main/java/contrail/correct/FastQtoAvro.java
@@ -1,17 +1,14 @@
 package contrail.correct;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.avro.Schema;
-import org.apache.avro.mapred.AvroJob; 
+import org.apache.avro.mapred.AvroJob;
 import org.apache.avro.mapred.AvroWrapper;
-import org.apache.avro.reflect.ReflectData;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -27,115 +24,103 @@ import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.RunningJob;
 import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.mapred.lib.NLineInputFormat;
 
 import contrail.sequences.FastQRecord;
 import contrail.stages.ContrailParameters;
 import contrail.stages.ParameterDefinition;
-import contrail.stages.RemoveTipsAvro;
 import contrail.stages.Stage;
 
 /*
  * This is a simple class used to convert Normal Fastq Data into the Avro format fastq data.
  * Some of this is similar to FastqPreprocessorAvroCompressed, which outputs a compressed
  * form of fastQ reads. We, however, need uncompressed strings for preprocessing stages
- * for flash and quake. It would be inefficient to convert fastQ files into compressed Avro 
- * fastQ files and reconvert it into uncompressed form for Flash/Quake processing. At the end of 
+ * for flash and quake. It would be inefficient to convert fastQ files into compressed Avro
+ * fastQ files and reconvert it into uncompressed form for Flash/Quake processing. At the end of
  * preprocessing stages, we will have normal, corrected fastQ files which can be fed into
  * FastqPreprocessorAvroCompressed.
- * 
- * This stage must be run separately for different kinds for reads, which are initially placed in 
- * different inputPaths. This is because, each type of serves as input at a particular stage 
+ *
+ * This stage must be run separately for different kinds for reads, which are initially placed in
+ * different inputPaths. This is because, each type of serves as input at a particular stage
  * of error correction. The different kinds of reads can be:
- * 
+ *
  * 1. Unpaired reads (no flash, only quake)
  * 2. Paired Reads 1 for Flash
  * 3. Paired Reads 2 for Flash
  * 4. Paired Reads 1 for Quake (no flash)
  * 5. Paired Reads 2 for Quake (no flash)
- * 
- *  * You can run this stage by specifying parameters in:
+ *
+ * You can run this stage by specifying parameters in:
  *  --inputpath=path/to/input --outputpath=path/to/store/avro/converted/files
+ *
+ * TODO(jeremy@lewi.us): We should update or replace this class which uses
+ * the new FastQ file input reader once that class is ready.
  */
+public class FastQtoAvro extends Stage {
 
-public class FastQtoAvro extends Stage
-{       
-  	
-  public static class FastqPreprocessorMapper extends MapReduceBase  implements Mapper<LongWritable, Text, AvroWrapper<FastQRecord>, NullWritable> 
-  {
+  public static class FastqPreprocessorMapper extends MapReduceBase
+      implements Mapper<LongWritable, Text, AvroWrapper<FastQRecord>, NullWritable> {
     private int idx = 0;
 
-    private String name = null;
     private String fastqId = null;
-    private String fastqRead = null; 
+    private String fastqRead = null;
     private String filename = null;
     private String fastqQvalue = null;
 
 
     private FastQRecord read = new FastQRecord();
-    private AvroWrapper<FastQRecord> out_wrapper = new AvroWrapper<FastQRecord>(read);
+    private AvroWrapper<FastQRecord> out_wrapper =
+        new AvroWrapper<FastQRecord>(read);
 
-    public void configure(JobConf job) 
-    {
-     
-    }
-/*
- * The mapper takes in key value pairs in the normal format and emits out Avro data
- */
-    public void map(LongWritable lineid, Text line, OutputCollector<AvroWrapper<FastQRecord>, NullWritable> output, Reporter reporter) throws IOException 
-    {
-
-      if (idx == 0) 
-      { 
+    /*
+     * The mapper takes in key value pairs in the normal format and emits out Avro data
+     */
+    public void map(
+        LongWritable lineid, Text line,
+        OutputCollector<AvroWrapper<FastQRecord>, NullWritable> output,
+        Reporter reporter) throws IOException {
+      if (idx == 0){
     	  fastqId = line.toString(); // The ID
       }
-      else if (idx == 1) {    
-    	
+      else if (idx == 1) {
     	  fastqRead = line.toString(); // The kmer read
       }
-      else if (idx == 2) { 
+      else if (idx == 2) {
     	  //plus sign - ignored
       }
-      else if (idx == 3)
-      {               
+      else if (idx == 3) {
     	  fastqQvalue = line.toString();
     	 /* Index of / is calculated since we need to make ids of mate pairs the same.
     	  *  We truncate everything after this /. This is required for the joining phases
     	  *  that follow. Two records (from mate pairs) with the same ID are joined for
-    	  *  flash/quake run 
+    	  *  flash/quake run
     	  */
     	 int ind = fastqId.lastIndexOf('/');
      	 if (ind==-1){
      		/*fastq not in proper format for mate pair reading
-     		 This condition can be handled explicitely if we want to 
-     		 preprocess only mate pairs, and data format is wrong somewhere 
+     		 This condition can be handled explicitely if we want to
+     		 preprocess only mate pairs, and data format is wrong somewhere
      		 for the mate pairs (ie. they miss /1 or /2)*/
      	 }
-     	 
+
      	 fastqId = fastqId.substring(1,ind);
     	 read.setId(fastqId);
     	 read.setQvalue(fastqQvalue);
     	 read.setRead(fastqRead);
-    	                                             
+
         output.collect(out_wrapper, NullWritable.get());
-
       }
-
       idx = (idx + 1) % 4;
     }
 
-    public void close() throws IOException
-    {
-      if (idx != 0)
-      {
+    public void close() throws IOException {
+      if (idx != 0){
     	  // Non multiple of 4  split
-        throw new IOException("ERROR: closing with idx = " + idx + " in " + filename);
+        throw new IOException(
+            "ERROR: closing with idx = " + idx + " in " + filename);
       }
     }
-
-	
   }
 
   /**
@@ -153,15 +138,14 @@ public class FastQtoAvro extends Stage
 	    }
 	    return Collections.unmodifiableMap(defs);
 	  }
-  
-  @Override      
-  public RunningJob runJob() throws Exception
-  { 
+
+  @Override
+  public RunningJob runJob() throws Exception {
     FastQRecord read = new FastQRecord();
-	Schema OUT_SCHEMA = read.getSchema();
-	String inputPath, outputPath;
-	inputPath = (String) stage_options.get("inputpath");
-	outputPath = (String) stage_options.get("outputpath");
+    Schema OUT_SCHEMA = read.getSchema();
+    String inputPath, outputPath;
+    inputPath = (String) stage_options.get("inputpath");
+    outputPath = (String) stage_options.get("outputpath");
     JobConf conf = new JobConf(FastQtoAvro.class);
     conf.setJobName("Conversion from text to Avro" + inputPath);
 
@@ -177,29 +161,29 @@ public class FastQtoAvro extends Stage
     conf.setNumReduceTasks(0);
 
     conf.setInputFormat(NLineInputFormat.class);
-    conf.setInt("mapred.line.input.format.linespermap", 2000000); // must be a multiple of 4
+    // linespermap must be a multiple of 4
+    conf.setInt("mapred.line.input.format.linespermap", 2000000);
 
     AvroJob.setOutputSchema(conf,OUT_SCHEMA);
 
     //delete the output directory if it exists already
     Path out_path = new Path(outputPath);
     if (FileSystem.get(conf).exists(out_path)) {
-      FileSystem.get(conf).delete(out_path, true);  
+      FileSystem.get(conf).delete(out_path, true);
     }
 
-    
-    long start_time = System.currentTimeMillis();    
+
+    long start_time = System.currentTimeMillis();
     RunningJob result = JobClient.runJob(conf);
-    long end_time = System.currentTimeMillis();    
+    long end_time = System.currentTimeMillis();
     double nseconds = (end_time - start_time) / 1000.0;
     System.out.println("Job took: " + nseconds + " seconds");
-    
+
     return result;
   }
 
-  	public static void main(String[] args) throws Exception {
-	    int res = ToolRunner.run(new Configuration(), new FastQtoAvro(), args);
-	    System.exit(res);
-	  }
-  
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new Configuration(), new FastQtoAvro(), args);
+    System.exit(res);
+  }
 }
