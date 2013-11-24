96a48a39c62835a6290fcad6c12230f08573e092
Jeremy Lewi <jeremy@lewi.us>
2013-11-23 17:26:49 -0800

    Print the directory the command gets executed from.

--- src/main/java/contrail/util/ShellUtil.java
+++ src/main/java/contrail/util/ShellUtil.java
@@ -138,6 +138,7 @@ public class ShellUtil {
     logger.info("Executing command: " + StringUtils.join(command, " "));
     ProcessBuilder builder = new ProcessBuilder(command);
     if ((directory != null) && (directory.length()) > 0) {
+      logger.info("Executing command in directory: " + directory);
       builder.directory(new File(directory));
     }
     return runProcess(
