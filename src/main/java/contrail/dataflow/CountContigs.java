73f3266929051b7406ababb0c4c571482a3bff91
Jeremy Lewi <jeremy@lewi.us>
2014-06-21 17:57:44 -0700

    Code is reading the GraphNodeData.

--- src/main/java/contrail/dataflow/CountContigs.java
+++ src/main/java/contrail/dataflow/CountContigs.java
@@ -1,11 +1,22 @@
 package contrail.dataflow;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.ByteChannel;
+import java.nio.channels.Channels;
 import java.util.ArrayList;
 
 import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileStream;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.reflect.ReflectData;
+import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.cloud.dataflow.sdk.Pipeline;
@@ -17,72 +28,67 @@ import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.DoFn.ProcessContext;
 import com.google.cloud.dataflow.sdk.transforms.PTransform;
 import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.google.cloud.dataflow.sdk.util.GcsUtil;
+import com.google.cloud.dataflow.sdk.util.GcsUtil.GcsFilename;
 import com.google.cloud.dataflow.sdk.values.PCollection;
 import com.google.cloud.dataflow.sdk.values.PCollectionList;
 
 import contrail.graph.GraphNodeData;
 import contrail.scaffolding.FilterReads;
+import contrail.stages.BuildGraphAvro;
 import contrail.stages.NonMRStage;
+import contrail.util.AvroSchemaUtil;
 
 public class CountContigs extends NonMRStage {
-//  static PCollectionList<String> makeSplits(
-//      boolean ordered, Pipeline p, Coder<GCSAvroFileSplit> coder, List<String>... lists) {
-//    List<PCollection<GCSAvroFileSplit>> inputs = new ArrayList<>();
-//    for (List<GCSAvroFileSplit> list : lists) {
-//      
-//      if (ordered) {
-//        pc.setOrdered(true);
-//      }
-//      pcs.add(pc);
-//    }
-//    return PCollectionList.of(pcs);
-//  }
-//  
-//  /** 
-//   * A PTransform to read avro files.
-//   */
-//  public static class ReadAvro<T> extends PTransform<PCollection<GCSAvroFileSplit>, PCollection<T>> {
-//    @Override
-//    public PCollection<String> apply(PCollection<String> lines) {
-//      
-//      // Convert lines of text into individual words.
-//      PCollection<String> words = lines.apply(
-//          ParDo.of(new ExtractWordsFn()));
-//
-//      // Count the number of times each word occurs.
-//      PCollection<KV<String, Long>> wordCounts =
-//          words.apply(Count.<String>create());
-//
-//      // Format each word and count into a printable string.
-//      PCollection<String> results = wordCounts.apply(
-//          ParDo.of(new FormatCountsFn()));
-//      
-//      PCollection<T> results = wordCounts.apply(
-//          ParDo.of(new FormatCountsFn()));
-//      return results;
-//    }
-//
-//    @Override
-//    public PCollection<T> apply(PCollection<GCSAvroFileSplit> input) {
-//      
-//      return results;
-//    }
-//  }
-  
+  private static final Logger sLogger = Logger.getLogger(CountContigs.class);  
   /** A DoFn that turns a GCSAvroFileSplit into elements. */
-  public static class ReadAvro<T> extends DoFn<GCSAvroFileSplit, String> {
+  public static class ReadAvro extends DoFn<GCSAvroFileSplit,GraphNodeData> {
+    private Class specificTypeClass;
+    
+    public ReadAvro(Class specificTypeClass) {
+      this.specificTypeClass = specificTypeClass;
+    }
+    
     @Override
     public void processElement(ProcessContext c) {
       GCSAvroFileSplit split = (GCSAvroFileSplit) c.element();
       System.out.println(split.getPath());
       
-      c.output("String");
-//      String[] words = c.element().split("[^a-zA-Z']+");
-//      for (String word : words) {
-//        if (!word.isEmpty()) {
-//          c.output(word);
-//        }
-//      }
+      GcsFilename gcsFilename = GcsUtil.asGcsFilename(split.getPath().toString());
+      
+      ByteChannel inChannel;
+      try {
+        GcsUtil gcsUtil = GcsUtil.create(c.getPipelineOptions());
+        inChannel = gcsUtil.open(gcsFilename);
+      } catch (IOException e1) {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+        return;
+      }
+      InputStream inStream = Channels.newInputStream(inChannel);
+      Schema schema = AvroSchemaUtil.getSchemaForSpecificType(
+          specificTypeClass);
+      SpecificDatumReader<GraphNodeData> datumReader = new SpecificDatumReader<GraphNodeData>(schema);
+
+      DataFileStream<GraphNodeData> fileReader;
+      try {
+        fileReader = new DataFileStream<GraphNodeData>(inStream, datumReader);
+      } catch(IOException e) {        
+        sLogger.error("Could not read file:" + split.getPath().toString());
+        return;
+      }
+      
+      while(fileReader.hasNext()) {
+        GraphNodeData datum = fileReader.next();
+        sLogger.info("read node");
+        c.output(datum);
+      }      
+      try {
+        fileReader.close();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
     }
   }
   
@@ -111,9 +117,7 @@ public class CountContigs extends NonMRStage {
     options.runner = "DirectPipelineRunner";
     
     Pipeline p = Pipeline.create();
-//
-//    AvroSpecificCoder<GCSAvroFileSplit> splitCoder = new AvroSpecificCoder<GCSAvroFileSplit>(new GCSAvroFileSplit().getSchema());
-//        AvroSpecificCoder<GraphNodeData> nodeCoder = new AvroSpecificCoder<GraphNodeData>(new GraphNodeData().getSchema());
+
     // Register default coders.
     p.getCoderRegistry().registerCoder(
         GCSAvroFileSplit.class, 
@@ -134,7 +138,9 @@ public class CountContigs extends NonMRStage {
     // is a specific Avro type? Do we need to write our own custom coder?
     PCollection<GCSAvroFileSplit> inputs = p.begin().apply(Create.of(splits));
     
-    inputs.apply(ParDo.of(new ReadAvro<GraphNodeData>()));
+    inputs
+      .apply(ParDo.of(new ReadAvro(GraphNodeData.class)))
+      .apply(ParDo.of(new GraphNodeDoFns.KeyByNodeId()));
 
     p.run(PipelineRunner.fromOptions(options));
   }
