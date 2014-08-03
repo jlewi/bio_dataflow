1015d154cb885efc7f496ca5b29931b27bdd7369
Jeremy Lewi <jeremy@lewi.us>
2014-08-03 22:19:15 +0000

    Initial refactoring of the code into a transform.
    The DataflowJob works with the DirectPipelineRunner.
    
    Parameters are still hardcoded.

--- src/main/java/contrail/dataflow/SubmitDataflowJob.java
+++ src/main/java/contrail/dataflow/SubmitDataflowJob.java
@@ -42,21 +42,32 @@ import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
 import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.coders.StringUtf8Coder;
+import com.google.cloud.dataflow.sdk.io.BigQueryIO;
 import com.google.cloud.dataflow.sdk.io.TextIO;
 import com.google.cloud.dataflow.sdk.runners.DataflowPipelineRunner;
 import com.google.cloud.dataflow.sdk.runners.PipelineOptions;
 import com.google.cloud.dataflow.sdk.runners.PipelineRunner;
+import com.google.cloud.dataflow.sdk.transforms.Create;
+import com.google.cloud.dataflow.sdk.transforms.CreatePObject;
 import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.google.cloud.dataflow.sdk.transforms.SeqDo.SeqDoFn;
 import com.google.cloud.dataflow.sdk.util.Credentials;
 import com.google.cloud.dataflow.sdk.util.GcsUtil;
 import com.google.cloud.dataflow.sdk.util.UserCodeException;
 import com.google.cloud.dataflow.sdk.values.PCollection;
+import com.google.cloud.dataflow.sdk.values.PObject;
+import com.google.cloud.dataflow.sdk.values.PObjectTuple;
+import com.google.cloud.dataflow.sdk.values.PObjectValueTuple;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.api.services.bigquery.model.TableSchema;
 import com.google.api.services.storage.Storage;
 import com.google.api.services.storage.model.StorageObject;
 import com.spotify.docker.client.DefaultDockerClient;
@@ -72,6 +83,7 @@ import com.spotify.docker.client.messages.ContainerInfo;
 import com.spotify.docker.client.messages.HostConfig;
 import com.spotify.docker.client.messages.PortBinding;
 
+import contrail.scaffolding.BowtieMapping;
 import contrail.sequences.FastUtil;
 import contrail.sequences.Read;
 import contrail.stages.ContrailParameters;
@@ -90,13 +102,7 @@ import contrail.util.ShellUtil;
 public class SubmitDataflowJob extends NonMRStage {
   private static final Logger sLogger = Logger.getLogger(
       SubmitDataflowJob.class);
-  
-  private DockerClient docker;
-  private String googleRegistryId;
-  private String localTempFileName;
-  
-  /* The local name of the registry. */
-  private String registryLocalName;
+ 
   
   /**
    *  creates the custom definitions that we need for this phase
@@ -107,244 +113,23 @@ public class SubmitDataflowJob extends NonMRStage {
         new HashMap<String, ParameterDefinition>();
     defs.putAll(super.createParameterDefinitions());
 
+    ContrailParameters.addList(defs, DataflowParameters.getDefinitions());
     return Collections.unmodifiableMap(defs);
   }
 
-  private void startDocker() {
-    // Docker must be using a tcp port to work with the spotify client.
-    String dockerAddress = "http://127.0.0.1:4243";
-    docker = new DefaultDockerClient(dockerAddress);    
-  }
-  
-  private void startGoogleRegistry() {
-    // Get the Google docker image.
-    String googleRegistryImage = "google/docker-registry";
-    try {
-      docker.pull(googleRegistryImage);
-    } catch (DockerRequestException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    } catch (InterruptedException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    } catch (DockerException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    
-    // TODO(jlewi): How to determine an unused port.
-    // TODO(jlewi): This assumes authorization is granted via a service account.
-    
-    // The google/docker-registry container is configured to use the port 5000.
-    String registryPort = "5000";
-    // The host can pick any available port.
-    // TODO(jlewi): Figure out how to pick an available port.
-    String hostPort = "5010";
-    
-    String exposedPort = registryPort + "/tcp";
-    ContainerConfig config = ContainerConfig.builder()
-        .image("google/docker-registry")
-        .env("GCS_BUCKET=biocloudops-docker")
-        .attachStderr(false)
-        .attachStdout(false)
-        .attachStdin(false)
-        .portSpecs(exposedPort)
-        //.exposedPorts(exposedPort)
-        .build();
-        
-    registryLocalName = "localhost:" + hostPort;
-    ContainerCreation creation;
-    // The id of the container running the google docker registry.
-    googleRegistryId = "";
-    try {
-      creation = docker.createContainer(config);
-      googleRegistryId = creation.id();
-      
-      Map<String, List<PortBinding>> portBindings = new TreeMap<String, List<PortBinding>>();
-      portBindings.put(exposedPort, new ArrayList<PortBinding>());
-      
-      // N.B. Trying to bind address 0.0.0.0 doesn't work. Maybe that address only works
-      // if you are connected via the unix socket and not a tcp port.
-      portBindings.get(exposedPort).add(PortBinding.of("127.0.0.1", Integer.parseInt(hostPort)));
-      
-      HostConfig hostConfig = HostConfig.builder().portBindings(portBindings).build();
-      docker.startContainer(googleRegistryId, hostConfig);
-      
-      while (!docker.inspectContainer(googleRegistryId).state().running()) {
-        sLogger.info("Waiting for google docker registry to start.");
-        Thread.sleep(1000);
-      }
-    }  catch (DockerRequestException e1) {
-      sLogger.error(e1.message(), e1);
-    } catch (DockerException e1) {      
-      sLogger.error(e1);        
-    } catch (InterruptedException e1) {
-      sLogger.error(e1);
-    }              
-  }
-  
-  /**
-   * Download the jar containing the user's code.
-   * 
-   * @param options
-   */
-  private void downloadJar(PipelineOptions options) {
-    JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-    String bucket = "biocloudops-temp";
-    String objectPath = "examples-1-20140730.jar";
-    
-    String[] pieces = objectPath.split("/");
-    String baseName = pieces[pieces.length - 1];
-    try {
-      File localTempFile;
-      localTempFile = File.createTempFile("tmp", baseName);
-      localTempFileName = localTempFile.getAbsolutePath();
-      localTempFile.delete();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-        
-    // Download the jar containing the code.
-    // Initialize the transport.    
-    NetHttpTransport httpTransport;
-    try {
-      httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-      // Authorization.
-      Credential credential = Credentials.getWorkerCredential(options);
-
-      // Set up global Storage instance.
-      Storage storage = new Storage.Builder(httpTransport, JSON_FACTORY, credential)
-      .setApplicationName(this.getClass().getSimpleName()).build();
-
-      Storage.Objects.Get getObject = storage.objects().get(bucket, objectPath);
-
-      // Downloading data.
-      FileOutputStream out = new FileOutputStream(localTempFileName);
-      // If you're not in AppEngine, download the whole thing in one request, if possible.
-      getObject.getMediaHttpDownloader();
-      getObject.executeMediaAndDownloadTo(out);
-
-      out.close();
-    } catch (GeneralSecurityException | IOException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-  }
-  
-  private void logLines(String[] lines) {
-    if (lines.length == 0) {
-      return;
-    }
-    
-    sLogger.info(StringUtils.join(lines, "\n"));
-  }
-  
-  protected void runJob() {
-    String imageName = registryLocalName + "/contrail/dataflow";    
-    List<String> serviceCommand = Arrays.asList(
-        "java", "-cp",  "/cloud-dataflow/target/examples-1.jar",
-        "com.google.cloud.dataflow.examples.WordCount",
-        "--runner", "BlockingDataflowPipelineRunner",
-        "--gcloudPath", "/google-cloud-sdk/bin/gcloud",
-        "--project", "biocloudops",
-        "--stagingLocation", "gs://dataflow-dogfood2-jlewi/staging",
-        "--input", "gs://dataflow-dogfood2-jlewi/nytimes-index.html",
-        "--output", "gs://dataflow-dogfood2-jlewi/tmp/nytimes-counts.txt");
-    
-    List<String> localCommand = Arrays.asList(
-        "java", "-cp",  "/cloud-dataflow/target/examples-1.jar",
-        "com.google.cloud.dataflow.examples.WordCount",
-        "--runner", "DirectPipelineRunner", "--input", "/tmp/words",
-        "--output", "/tmp/word-count.txt");
-    
-    ContainerConfig config = ContainerConfig.builder()
-        .image(imageName)
-        .cmd(serviceCommand)
-        .attachStderr(true)
-        .attachStderr(true)
-        .build();
-        
-    ContainerCreation creation;
-    try {
-      // Fetch the image.
-      // TODO(jeremy@lewi.us): I think there might be a race condition to
-      // make sure the google-docker registry containter is fully started.
-      int numTries = 0;
-      final int maxRetries = 5;
-      while (true) {
-        ++numTries;
-        try {
-          docker.pull(imageName);
-          break;
-        } catch (DockerRequestException e) {
-          if (numTries < maxRetries) {
-            sLogger.info("Waiting for Google Docker Regiistry to start...");
-            Thread.sleep(500);            
-          } else {
-            throw e;
-          }
-        }
-      }
-      creation = docker.createContainer(config);
-      String id = creation.id();
-      ContainerInfo info = docker.inspectContainer(id);
-      docker.startContainer(id);
-      //docker.waitContainer(id);
-          
-      // N.B. We don't get a single stream with both logs because
-      // we want to keep track of how many lines we've read and only 
-      // print the difference. Docker's FOLLOW capability won't seem to work with 
-      // the remote API. 
-      ContainerLogStream stdOutStream = new ContainerLogStream(
-          docker, ContainerLogStream.DockerStream.STDOUT, id);
-      ContainerLogStream stdErrStream = new ContainerLogStream(
-          docker, ContainerLogStream.DockerStream.STDERR, id);
-      while (docker.inspectContainer(id).state().running()) {      
-        LogStream stdErr = docker.logs(id, LogsParameter.STDERR);
-
-        logLines(stdOutStream.readNextLines());
-        logLines(stdErrStream.readNextLines());
-               
-        // Sleep for a .25 seconds.
-        Thread.sleep(250);
-      }
-      logLines(stdOutStream.readNextLines());
-      logLines(stdErrStream.readNextLines());
-      
-      // Remove the container.
-      docker.removeContainer(id);
-    } catch (DockerRequestException e) {
-      sLogger.error(e.message(), e);
-    } catch (DockerException | InterruptedException e) {
-    	
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }    
-  }
-  
   @Override
   protected void stageMain() {
-    PipelineOptions options = new PipelineOptions();
-       
-    downloadJar(options);
- 
-    startDocker();
-    startGoogleRegistry();
+    PipelineOptions options = new PipelineOptions();       
+    DataflowParameters.setPipelineOptions(stage_options, options);
+
+    Pipeline p = Pipeline.create();
+
     
-    runJob();
+    PObject<String> jobSpec = p.begin().apply(CreatePObject.of("hel)o"))
+        .setCoder(StringUtf8Coder.of());
     
-    // Stop the registry.
-    // TODO(jlewi): We need to figure out how to make sure this always runs
-    // to avoid leaving the google registry running.
-    try {
-      docker.killContainer(googleRegistryId);
-      docker.removeContainer(googleRegistryId);
-    } catch (DockerException | InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } 
+    jobSpec.apply(new RunDataflowJob());
+    p.run(PipelineRunner.fromOptions(options));    
   }
 
   public static void main(String[] args) throws Exception {
